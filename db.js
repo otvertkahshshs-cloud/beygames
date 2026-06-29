bb7b39ac44789c5c5bb7285c25b0d91f13aca9e6
otvertkahahshs <goshamal@bk.ru>
2026-06-29 23:13:00 +0300

    fix(db): leave only CS:GO section in seed

--- db.js
+++ db.js
@@ -132,22 +132,11 @@ async function init() {
     const cats = db.prepare('SELECT id FROM cats LIMIT 1').get();
     if (cats) return;
 
-    const c1 = uuidv4(), c2 = uuidv4(), c3 = uuidv4(), c4 = uuidv4();
+    const c1 = uuidv4();
     db.prepare('INSERT INTO cats VALUES (?,?,?,?,?)').run(c1,'Читы и Хаки','Всё для игровых читов','<i class="fa-solid fa-crosshairs"></i>',1);
-    db.prepare('INSERT INTO cats VALUES (?,?,?,?,?)').run(c2,'Игры','Обсуждение игр','<i class="fa-solid fa-gamepad"></i>',2);
-    db.prepare('INSERT INTO cats VALUES (?,?,?,?,?)').run(c3,'Маркет','Купля и продажа','<i class="fa-solid fa-tag"></i>',3);
-    db.prepare('INSERT INTO cats VALUES (?,?,?,?,?)').run(c4,'Общение','Флуд и общение','<i class="fa-solid fa-comments"></i>',4);
 
     const secs = [
-        [uuidv4(),c1,'CS2','Читы для CS2','<i class="fa-solid fa-gun"></i>',1],
-        [uuidv4(),c1,'Valorant','Читы для Valorant','<i class="fa-solid fa-bolt"></i>',2],
-        [uuidv4(),c1,'Rust','Читы для Rust','<i class="fa-solid fa-biohazard"></i>',3],
-        [uuidv4(),c2,'CS2','Обсуждение CS2','<i class="fa-solid fa-gun"></i>',1],
-        [uuidv4(),c2,'Другие игры','Прочие игры','<i class="fa-solid fa-dice"></i>',2],
-        [uuidv4(),c3,'Аккаунты','Продажа аккаунтов','<i class="fa-solid fa-user"></i>',1],
-        [uuidv4(),c3,'Услуги','Игровые услуги','<i class="fa-solid fa-wrench"></i>',2],
-        [uuidv4(),c4,'Флудилка','Общение обо всём','<i class="fa-solid fa-fire"></i>',1],
-        [uuidv4(),c4,'Знакомства','Найди друзей','<i class="fa-solid fa-handshake"></i>',2],
+        [uuidv4(),c1,'Counter-Strike: Global Offensive','Читы для CS:GO','<i class="fa-solid fa-gun"></i>',1],
     ];
     for (const s of secs)
         db.prepare('INSERT INTO sections(id,cat_id,name,description,icon,sort,threads_count,posts_count) VALUES(?,?,?,?,?,?,0,0)').run(...s);
